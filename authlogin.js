e1eb39a82f944d6ec4b67e2e8fea43c95c796f98
fmalematser <ahhmebattiahut@gmail.com>
2025-06-01 13:41:36 -0400

    Create authlogin.js

---
+++ authlogin.js
@@ -0,0 +1,12 @@
+const firebaseConfig = {
+  apiKey: "AIzaSyB0e8-U4nb8sPyZaw1_S2bD2wub2xJlYP4",
+  authDomain: "darkmodz2.firebaseapp.com",
+  projectId: "darkmodz2",
+  storageBucket: "darkmodz2.appspot.com",
+  messagingSenderId: "730284042393",
+  appId: "1:730284042393:web:18e4d5518050f1e7c57008",
+  measurementId: "G-S25SZ1G07Q"
+};
+
+firebase.initializeApp(firebaseConfig);
+const auth = firebase.auth();
